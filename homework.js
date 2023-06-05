a72643240ae5b40baf9e21801efffa13306c443b
DEdmonds22 <ddedmonds22@gmail.com>
2023-06-05 13:56:49 -0400

    created hamster class

--- homework.js
+++ homework.js
@@ -1,1 +1,19 @@
-console.log("test");
+class Hamster {
+    constructor(owner = "", name, price = 15) {
+        this.owner = owner;
+        this.name = name;
+        this.price = price;
+    }
+
+    wheelRun() {
+        console.log("squak squeak");
+    }
+
+    eatFood() {
+        console.log("nibble nibble");
+    }
+
+    getPrice() {
+        return this.price;
+    }
+}
