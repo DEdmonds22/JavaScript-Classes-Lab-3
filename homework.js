1eaaf92688fa8ac56d3751367377ba15541dfecc
DEdmonds22 <ddedmonds22@gmail.com>
2023-06-06 15:18:37 -0400

    created person class

--- homework.js
+++ homework.js
@@ -17,3 +17,54 @@ class Hamster {
         return this.price;
     }
 }
+
+class Person {
+    constructor(name, age = 0, height = 0, weight = 0, mood = 0, hamsters = [], bankAccount = 0){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.mood = mood;
+        this.hamsters = hamsters;
+        this.bankAccount = bankAccount;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getAge() {
+        return this.age;
+    }
+
+    getWeight() {
+        return this.weight;
+    }
+
+    greet() {
+        return `Hello ${this.name}`;
+    }
+
+    eat() {
+        this.weight++;
+        this.mood++;
+    }
+
+    exercise() {
+        this.weight--;
+    }
+
+    ageUp() {
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankAccount++;
+    }
+
+    buyHamster(hamster) {
+        this.hamsters.push(hamster);
+        this.mood += 10;
+        this.bankAccount -= Hamster.getPrice();
+    }
+}
